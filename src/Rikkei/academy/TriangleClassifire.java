a676b3c8295f7f0c17329342c6ae020e15dbc553
3K3ki <danghung090@gmail.com>
2023-03-21 10:17:30 +0700

    secc

--- src/Rikkei/academy/TriangleClassifire.java
+++ src/Rikkei/academy/TriangleClassifire.java
@@ -4,10 +4,10 @@ public class TriangleClassifire {
     public static String triangleClassifire(int a, int b, int c) {
         if (a + b > c && a + c > b && b + c > a) {
             if (a == b && b == c) {
-                return "tam giác đều";
+                return "tam giác cân";
             }
             if (a == b || b == c || c == a) {
-                return "tam giác cân";
+                return "tam giác đều";
             }
             return "tam giác thường";
 
